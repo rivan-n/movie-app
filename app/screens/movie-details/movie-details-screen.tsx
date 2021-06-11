ba2dd94df22c40a1460d999180826100859cedd8
Rivan Novriaji <rivan.novriaji@photoninfotech.net>
2021-06-11 09:53:53 +0700

    Develop movies details screen

--- app/screens/movie-details/movie-details-screen.tsx
+++ app/screens/movie-details/movie-details-screen.tsx
@@ -1,26 +1,50 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
-import { View } from "react-native"
+import { Dimensions, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import { useNavigation } from "@react-navigation/native"
-import { Button, Header, Screen, Wallpaper } from "../../components"
+import FastImage from "react-native-fast-image"
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import { Button, Header, Screen, Text, Wallpaper } from "../../components"
+import { calculateContainerHeight } from "../../utils/screen"
 import { styles } from "./style"
 
-export const MovieDetailsScreen = observer(function MovieDetailsScreen() {
+export const MovieDetailsScreen = observer(function MovieDetailsScreen({ route }) {
     const navigation = useNavigation()
 
     const goBack = () => navigation.goBack()
 
+    const { movie } = route.params
+
+    //TODO: Pull imageBaseUrl from configuration service
+    const imageBaseUrl = "https://image.tmdb.org/t/p/w1280"
+
+    const screenWidth = Dimensions.get("screen").width
+    const height = calculateContainerHeight(screenWidth, 1280, 720)
+
+    const releaseYear = movie.release_date ? movie.release_date.substring(0, 4) : null
+
     return (
         <Screen style={styles.screen}>
             <Wallpaper />
-            <Header headerTx={'movieDetailsScreen.title'} leftIcon="back" onLeftPress={goBack} />
+            <Header leftIcon="back" onLeftPress={goBack} />
             <View style={styles.container}>
+                <FastImage
+                    style={{ width: screenWidth, height: height }}
+                    resizeMode="contain"
+                    source={{ uri: `${imageBaseUrl}${movie.backdrop_path}` }}
+                />
+                <Text preset="header">{movie.title} <Text preset="fieldLabel">{releaseYear ? `(${releaseYear})` : null}</Text></Text>
+                <Text>{movie.overview}</Text>
                 <View />
                 <SafeAreaView edges={["left", "right", "bottom"]}>
                     <Button
-                        testID="next-screen-button"
-                        tx="welcomeScreen.continue"
+                        testID="add-to-watch-list-button"
+                        children={(
+                            <Text preset="bold">
+                                <MaterialIcons name="playlist-add" size={15} /> <Text tx={"movieDetailsScreen.addToWatchList"} />
+                            </Text>
+                        )}
                         onPress={goBack}
                     />
                 </SafeAreaView>
